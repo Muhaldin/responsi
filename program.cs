ebe2d56b1c6c9108d592b9bd26529545696fb42b
Muhaldin <ode.al@students.amikom.ac.id>
2020-04-15 12:24:23 +0700

    first commit

---
+++ program.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace ResponsiPemrograman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nama     : La Ode Muhammad Aldin");
+            Console.WriteLine("NIM      : 19.11.2807");
+            Console.WriteLine("Kelas    : 19 IF 04");
+
+            Console.WriteLine("\n");
+
+
+            Karyawan karyawan1 = new Karyawan(1911, "Muh Aldin", 5000000);
+            Karyawan karyawan2 = new Karyawan(2742, "fitra", 3000000);
+            Karyawan karyawan3 = new Karyawan(1519, "piang", 100000);
+
+
+            Console.WriteLine("==================================================");
+            Console.WriteLine(" NO | NIK / NAMA\t\t | Gaji Bulanan\t |");
+            Console.WriteLine("==================================================");
+            Console.WriteLine(" 1. | {0} {1} \t\t | {2}\t |", karyawan1.NIK, karyawan1.Nama, karyawan1.GajiBulanan);
+            Console.WriteLine(" 2. | {0} {1} \t\t | {2}\t |", karyawan2.NIK, karyawan2.Nama, karyawan2.GajiBulanan);
+            Console.WriteLine(" 3. | {0} {1} \t\t | {2}\t\t |", karyawan3.NIK, karyawan3.Nama, karyawan3.GajiBulanan);
+
+            Console.WriteLine("\n\nAsikkkkk dpt kenaikan gaji nih!!!\n");
+
+            Console.WriteLine("==================================================");
+            Console.WriteLine(" NO | NIK / NAMA\t\t | Gaji Bulanan\t |");
+            Console.WriteLine("==================================================");
+            Console.WriteLine(" 1. | {0} {1} \t\t | {2}\t |", karyawan1.NIK, karyawan1.Nama, karyawan1.NaikGaji);
+            Console.WriteLine(" 2. | {0} {1} \t\t | {2}\t |", karyawan2.NIK, karyawan2.Nama, karyawan2.NaikGaji);
+            Console.WriteLine(" 3. | {0} {1} \t\t | {2}\t\t |", karyawan3.NIK, karyawan3.Nama, karyawan3.GajiBulanan);
+
+
+            Console.ReadKey();
+        }
+    }
+
+}
